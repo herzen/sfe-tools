48680a358acf36db56fa7dcd79e977e5aca50db5
Alex Viskovatoff <herzen@imap.cc>
2013-11-24 10:24:18 -0500

    Tweak regexes

--- scripts/get-sfe-ips-names.pl
+++ scripts/get-sfe-ips-names.pl
@@ -1,9 +1,7 @@
 #!/usr/bin/perl
 use v5.12;
 
-my @lines = `grep -i ips_package_name *.spec encumbered/*.spec`;
-
-for (@lines) {
-    m{^(SFE[a-zA-Z_0-9\-]+\.spec):\w+:\s+(\S+)\n};
+for ( `grep -i '^ips_package_name' *.spec encumbered/*.spec` ) {
+    m{(SFE[a-zA-Z_0-9\-\.]+\.spec):\w+:\s+(\S+)\s+};
     printf "%-50s %s\n", $2, $1;
 }
