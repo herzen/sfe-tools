d76c312e75b8d09b2194636bca4b3360982b5fab
Alex Viskovatoff <herzen@imap.cc>
2013-11-24 14:04:49 -0500

    No need to include filename extension in SFE spec name

--- scripts/get-sfe-ips-names.pl
+++ scripts/get-sfe-ips-names.pl
@@ -1,7 +1,13 @@
 #!/usr/bin/perl
 use v5.12;
 
+=pod
+This script goes through SFE spec files and, if they declare an IPS package
+name, prints out that name and the old SFE package name.
+It expects to be run in the SFE directory.
+=cut
+
 for ( `grep -i '^ips_package_name' *.spec encumbered/*.spec` ) {
-    m{(SFE[a-zA-Z_0-9\-\.]+\.spec):\w+:\s+(\S+)\s+};
+    m{(SFE[a-zA-Z_0-9\-\.]+)\.spec:\w+:\s+(\S+)\s+};
     printf "%-50s %s\n", $2, $1;
 }
