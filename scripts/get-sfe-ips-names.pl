49b87597efbf50d9776d9f5cc84d2fe598b6f4e8
Alex Viskovatoff <herzen@imap.cc>
2014-02-25 23:41:47 -0500

    Make get-sfe-ips-names.pl use spectool.pl to process specs.
    This allows IPS name declarations which use a pkgbuild macro to get
    resolved, something that did not happen when the raw output from grep
    was used.
    
    TODO: check that pkgbuild is installed and do not hard code its version
    into the path of its libraries.

--- scripts/get-sfe-ips-names.pl
+++ scripts/get-sfe-ips-names.pl
@@ -3,11 +3,16 @@ use v5.12;
 
 =pod
 This script goes through SFE spec files and, if they declare an IPS package
-name, prints out that name and the old SFE package name.
+name, prints out that name preceded by the old SFE package name.
 It expects to be run in the SFE directory.
 =cut
 
-for ( `grep -i '^ips_package_name' *.spec encumbered/*.spec` ) {
-    m{(SFE[a-zA-Z_0-9\-\.]+)\.spec:\w+:\s+(\S+)\s+};
-    printf "%-50s %s\n", $2, $1;
-}
+my @matches =  `grep -i '^ips_package_name' *.spec encumbered/*.spec`;
+my @spec_names = map {m|([a-zA-Z_0-9\-\./]+\.spec):|; $1} @matches;
+
+unshift @INC, "/usr/lib/pkgbuild-1.3.105";
+require "spectool.pl";
+process_defaults ();
+for my $spec_name (@spec_names) { read_spec ($spec_name); }
+
+do_get_package_names ();
