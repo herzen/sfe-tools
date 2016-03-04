aa73779bde6314be15a295904c735d3d9a715b8b
Alex Viskovatoff <herzen@imap.cc>
2016-03-03 20:30:19 -0500

    Use own get_buildrequires for the sake of simplification

--- scripts/pkgnmmap.pl
+++ scripts/pkgnmmap.pl
@@ -4,29 +4,39 @@ use v5.10;
 use constant PKGBUILD_ROOT => '/opt/dtbld/';
 use lib PKGBUILD_ROOT . 'lib/pkgbuild-1.3.105';
 
-# These are defined in spectool.pl.  Defining them here does not affect that,
-# unless we make these "our" variables.
-#my $build_engine = PKGBUILD_ROOT . 'bin/pkgbuild';
-#my $_pkgbuild_path = PKGBUILD_ROOT . 'lib/pkgbuild-1.3.105';
+our @specs = ();
 
 # Setting this variable inhibits init and main being called in spectool.pl
 our $run_from_pkgnmmap = 1;
 require 'spectool.pl';
 
+sub get_buildrequires () {
+    my $spec = $specs[0];
+    if (defined $spec->{error}) {
+	msg_error ($spec->get_base_file_name () . ": " . $spec->{error});
+	$exit_val++;
+    } else {
+	my @pkgs = $spec->get_packages ();
+	my @buildreqs = ();
+	my $pkg = $pkgs[0];
+	my @pkg_breqs = $pkg->get_array ('buildrequires');
+	if (@pkg_breqs) {
+	    push(@buildreqs, @pkg_breqs);
+	}
+	for (@buildreqs) { say $_ ; }
+    }
+}
 
 sub mymain {
     process_defaults ();
 
-    read_spec("pkgnmmap_pnm.spec");
+    read_spec("pkgnmmap.pnms");
 
     process_specs ();
-    do_get_buildrequires ();
+    get_buildrequires ();
 
     exit ($exit_val);
 }
-
-# Ignore any arguments that were supplied on the command line for now
-#$pkgbuild_path = shift (@ARGV);
 
 init;
 mymain;
