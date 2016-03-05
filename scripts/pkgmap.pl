5b6b4aa0b60fa7e08b28733a40330d707be1b126
Alex Viskovatoff <herzen@imap.cc>
2016-03-05 15:37:31 -0500

    Create pkgmap.pl, which produces a yaml dump of package mappings from a csv file

---
+++ scripts/pkgmap.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+use v5.12;
+use autodie;
+use YAML::XS qw(Dump);
+
+my $csv_filename = '../data/mapping.csv';
+my $yaml_filename = '../data/mapping.yaml';
+my @col_titles;
+my %mappings;
+my $monopkgs = 0;
+my $multipkgs = 0;
+
+sub read_csv_file {
+    open CSVS, '<', shift;
+    my $line = <CSVS>;
+    chomp $line;
+    @col_titles = split /,/, $line;
+    while ($line = <CSVS>) {
+	chomp $line;
+	my ($key, @list) = split /,/, $line;
+	if (members_eq(@list)) {
+	    $mappings{$key} = $list[0];
+	    $monopkgs++;
+	} else {
+	    $mappings{$key} = \@list;
+	    $multipkgs++;
+	}
+    }
+}
+
+# We are all Haskell programmers now
+sub members_eq {
+    my ($head, @tail) = @_;
+    if (!@tail) {
+	return 1;
+    }
+    elsif ($head ne $tail[0]) {
+	return 0;
+    }
+    else { members_eq (@tail); }
+}
+
+
+read_csv_file ($csv_filename);
+say "$multipkgs packages with different names; $monopkgs with the same name";
+
+open YAMLS, '>', $yaml_filename;
+print YAMLS Dump( \@col_titles, \%mappings );
