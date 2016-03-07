91c5f05cc31fb57e598c58cc32e9f845c8bc59aa
Alex Viskovatoff <herzen@imap.cc>
2016-03-07 12:20:05 -0500

    Process "Requires:" to create mapping.csv, not just "BuildRequires:"

--- scripts/get-pn-symbols.pl
+++ scripts/get-pn-symbols.pl
@@ -6,7 +6,7 @@ Get list of all package name macros referred to by BuildRequires in all
 SFE specs, removing the leading "pnm_buildrequires_" from the symbols.
 =cut
 
-my @matches = `ggrep '^BuildRequires:[[:space:]]\\+%{pnm_' *.spec encumbered/*.spec`;
+my @matches = `ggrep '^\\(Build\\)\\?Requires:[[:space:]]\\+%{pnm_' *.spec encumbered/*.spec`;
 my %pkg_names;
 
 for (@matches) {
